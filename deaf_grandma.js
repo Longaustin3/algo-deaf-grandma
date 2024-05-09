bab41c9262ec958b730f18db5c90274d607aac9f
Austin Long <longaustin3@gmail.com>
2024-05-09 12:44:22 -0400

    cleaned up code

--- deaf_grandma.js
+++ deaf_grandma.js
@@ -1,5 +1,39 @@
 function deafGrandma() {
 
+    let goodbyCounter = 0
+    let input = ""
+    let response = ""
+
+    // grandma start conversation
+    alert("HEY KID!")
+
+    while (goodbyCounter <= 1) { // continue talking to grandma so long as we haven't said goodbye more than once
+
+        input = window.prompt("Respond to grandma:")
+
+        // say nothing
+        if (input === "") {
+            response = "WHAT?!"
+            // any lower case in input
+        } else if (input.toUpperCase() !== input) {
+            response = "SPEAK UP, KID!"
+            // normal question in all caps
+        } else if (input === "GOODBYE!") {
+            goodbyCounter++
+
+            if (goodbyCounter > 1) { // we've said goobye once before
+                response = "LATER, SKATER!"
+            }
+            else { // this is the first time we're saying goodbye
+                response = "LEAVING SO SOON?"
+            }
+
+        } else {
+            response = "NO, NOT SINCE 1946!"
+        }
+
+        alert(response)
+    }
 }
 
 deafGrandma();
